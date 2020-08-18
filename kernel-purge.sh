f5c394327435996ac735eed3d489267905a44cc5
Doug Hoard <doug.hoard@gmail.com>
2020-08-18 11:24:44 -0400

    Added headless mode to kernel-purge.sh

--- kernel-purge.sh
+++ kernel-purge.sh
@@ -6,20 +6,28 @@ current_kernel="$(uname -r | sed 's/\(.*\)-\([^0-9]\+\)/\1/')"
 current_ver=${current_kernel/%-generic}
 
 echo "Running kernel version is: ${current_kernel}"
-# uname -a
 
 function xpkg_list() {
     dpkg -l 'linux-*' | sed '/^ii/!d;/linux-libc-dev/d;/'${current_ver}'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d'
 }
 
-echo "The following (unused) KERNEL packages will be removed:"
-xpkg_list
-
-read -p 'Do you want to continue [yN]? ' -n 1 -r
-printf "\n"
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ $# -eq 0 ]];
 then
+    echo "The following (unused) KERNEL packages will be removed:"
+    xpkg_list
+    
+    read -p 'Do you want to continue [yN]? ' -n 1 -r
+    printf "\n"
+    
+    if [[ $REPLY =~ ^[Yy]$ ]];
+    then
+        xpkg_list | xargs sudo apt-get -y purge
+    else
+        echo 'Operation aborted. No changes were made.'
+    fi
+elif [ "$1" = "headless" ];
+then
+    echo "The following (unused) KERNEL packages will be removed:"
+    xpkg_list
     xpkg_list | xargs sudo apt-get -y purge
-else
-    echo 'Operation aborted. No changes were made.'
 fi
