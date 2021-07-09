732b7fb5f9743f3b41db2769ddebeb5447d9a6cc
Doug Hoard <doug.hoard@gmail.com>
2021-07-09 14:37:04 -0400

    Added apt-get upgrade options

--- update.sh
+++ update.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt -y autoremove
+source /etc/os-release
+
+if [ "Ubuntu" == "$NAME" ]; then
+  echo "Ubuntu"
+elif [ "Linux Mint" == "$NAME" ]; then
+  echo "Linux Mint"
+else
+  echo "Unknown"
+  exit 1
+fi
+
+sudo apt-get -y update -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt autoremove -y
 
 if [ -f /var/run/reboot-required ]; then
   echo 'reboot required'
