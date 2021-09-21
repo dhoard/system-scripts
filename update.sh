da4358f2e0767891a2f4ebc4a5d6a6079d5fca4d
Doug Hoard <doug.hoard@gmail.com>
2021-09-21 12:46:41 -0400

    Added support for Raspbian

--- update.sh
+++ update.sh
@@ -6,15 +6,22 @@ if [ "Ubuntu" == "$NAME" ]; then
   echo "Ubuntu"
 elif [ "Linux Mint" == "$NAME" ]; then
   echo "Linux Mint"
+elif [ "Raspbian GNU/Linux" == "$NAME" ]; then
+  echo "Raspbian GNX/Linux"
 else
   echo "Unknown"
   exit 1
 fi
 
-apt-get -y update -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'
+if [ "Raspbian GNU/Linux" == "$NAME" ]; then
+  apt-get -y update
+else
+  apt-get -y update -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'
+fi
+
 apt-get -y upgrade
 apt-get -y dist-upgrade
-apt autoremove -y
+apt-get -y autoremove
 
 if [ -f /var/run/reboot-required ]; then
   echo 'reboot required'
