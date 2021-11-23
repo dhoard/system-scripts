5f3f17ab7f17feb4c8f01831f2e178e5462963cd
Doug Hoard <doug.hoard@gmail.com>
2021-11-23 13:05:31 -0500

    Fixed random temp file name

--- run-on-hosts.sh
+++ run-on-hosts.sh
@@ -2,7 +2,8 @@
 
 if [ ! -f "${2}" ]
 then
-  FILE=$(mktemp /tmp/run-on-hosts.XXXXXXXXX)
+  SUFFIX=$RANDOM$RANDOM
+  FILE=($mktemp /tmp/run-on-hosts.${SUFFIX})
   echo "${2}" > ${FILE}
 else
   FILE="${2}"
