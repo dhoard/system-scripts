7b889b07d16d4015760d8de3f50d653fa40cef0a
Doug Hoard <doug.hoard@gmail.com>
2022-02-24 07:49:21 -0500

    Added AlmaLinux support

--- check-upgradable.sh
+++ check-upgradable.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-apt-get update
-apt-get -u upgrade --assume-no
+source /etc/os-release
+TYPE=Unknown
+
+if [ "Ubuntu" == "$NAME" ]; then
+  echo "Ubuntu"
+  TYPE=apt
+elif [ "Linux Mint" == "$NAME" ]; then
+  echo "Linux Mint"
+  TYPE=apt
+elif [ "Raspbian GNU/Linux" == "$NAME" ]; then
+  echo "Raspbian GNU/Linux"
+  TYPE=apt
+elif [ "AlmaLinux" = "$NAME" ]; then
+  echo "AlmaLinux"
+  TYPE=dnf
+else
+  echo "Unknown"
+  exit 1
+fi
+
+if [ "apt" == "$TYPE" ]; then
+  apt-get update
+  apt-get -u upgrade --assume-no
+fi
+
+if [ "dnf" == "$TYPE" ]; then
+  dnf list updates
+fi
