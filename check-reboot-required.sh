f72775f688233eb7ff5d9ed0cf49df2d18c5228f
Doug Hoard <doug.hoard@gmail.com>
2022-03-21 10:46:38 -0400

    Added support for RHEL 8

--- check-reboot-required.sh
+++ check-reboot-required.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
-if [ -f /var/run/reboot-required ]; then
-  echo 'reboot required'
+source /etc/os-release
+PACKAGE_MANAGER=Unknown
+
+if [ "Ubuntu" == "$NAME" ]; then
+  echo "Ubuntu"
+  PACKAGE_MANAGER=apt
+elif [ "Linux Mint" == "$NAME" ]; then
+  echo "Linux Mint"
+  PACKAGE_MANAGER=apt
+elif [ "Raspbian GNU/Linux" == "$NAME" ]; then
+  echo "Raspbian GNU/Linux"
+  PACKAGE_MANAGER=apt
+elif [ "AlmaLinux" = "$NAME" ]; then
+  echo "AlmaLinux"
+  PACKAGE_MANAGER=dnf
+else
+  echo "Unknown Linux type"
+  exit 1
+fi
+
+if [ "apt" == "$PACKAGE_MANAGER" ]; then
+  if [ -f /var/run/reboot-required ]; then
+    echo "reboot required"
+  fi
+fi
+
+if [ "dnf" == "$PACKAGE_MANAGER" ]; then
+  OUTPUT=`needs-restarting -r | grep "Reboot should not be necessary."`
+  if [ "$OUTPUT" != "Reboot should not be necessary." ]; then
+    echo "reboot required"a
+  fi
 fi
