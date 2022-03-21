92a7149dfcfa6d9a13e0aaf0a2db4f393aa27210
Doug Hoard <doug.hoard@gmail.com>
2022-03-21 11:13:00 -0400

    Added code to check if tune.sh has been ran

--- tune.sh
+++ tune.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-echo "#" >> /etc/sysctl.conf
-echo "# tune.sh" >> /etc/sysctl.conf
-echo "#" >> /etc/sysctl.conf
+OUTPUT=`grep "# tune.sh" /etc/systctl.conf`
 
-echo net.core.rmem_max=16777216 >> /etc/sysctl.conf
-echo net.core.wmem_max=16777216 >> /etc/sysctl.conf
-echo net.ipv4.tcp_rmem=4096 87380 16777216 >> /etc/sysctl.conf
-echo net.ipv4.tcp_wmem=4096 65536 16777216 >> /etc/sysctl.conf
-echo 'vm.swappiness=5' >> /etc/sysctl.conf
-echo 'vm.vfs_cache_pressure=50' >> /etc/sysctl.conf
+if [ "$OUTPUT" == "" ]; then
 
-sysctl -p
+  echo "#" >> /etc/sysctl.conf
+  echo "# tune.sh" >> /etc/sysctl.conf
+  echo "#" >> /etc/sysctl.conf
 
-echo 0 > /sys/block/vda/queue/rotational
-echo 0 > /sys/block/vda/queue/rq_affinity
-echo "none" > /sys/block/vda/queue/scheduler
+  echo net.core.rmem_max=16777216 >> /etc/sysctl.conf
+  echo net.core.wmem_max=16777216 >> /etc/sysctl.conf
+  echo net.ipv4.tcp_rmem=4096 87380 16777216 >> /etc/sysctl.conf
+  echo net.ipv4.tcp_wmem=4096 65536 16777216 >> /etc/sysctl.conf
+  echo 'vm.swappiness=5' >> /etc/sysctl.conf
+  echo 'vm.vfs_cache_pressure=50' >> /etc/sysctl.conf
 
-echo "#" >> /etc/rc.local
-echo "# tune.sh" >> /etc/rc.local
-echo "#" >> /etc/rc.local
+  sysctl -p
 
-echo "echo 0 > /sys/block/vda/queue/rotational" >> /etc/rc.local
-echo "echo 0 > /sys/block/vda/queue/rq_affinity" >> /etc/rc.local
-echo "echo none > /sys/block/vda/queue/scheduler" >> /etc/rc.local
+  echo 0 > /sys/block/vda/queue/rotational
+  echo 0 > /sys/block/vda/queue/rq_affinity
+  echo "none" > /sys/block/vda/queue/scheduler
 
-#chmod u+x /etc/rc.local
-#systemctl enable rc-local
-#systemctl restart rc-local
-#systemctl restart rc-local
+  #echo "#" >> /etc/rc.local
+  #echo "# tune.sh" >> /etc/rc.local
+  #echo "#" >> /etc/rc.local
+
+  echo "echo 0 > /sys/block/vda/queue/rotational" >> /etc/rc.local
+  echo "echo 0 > /sys/block/vda/queue/rq_affinity" >> /etc/rc.local
+  echo "echo none > /sys/block/vda/queue/scheduler" >> /etc/rc.local
+
+  #chmod u+x /etc/rc.local
+  #systemctl enable rc-local
+  #systemctl restart rc-local
+  #systemctl restart rc-local
+
+fi
